21802ec18f4a3c99bcada9718049f42e7064cc6a
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-18 02:18:02 +0530

    Update AndroidSupportMeWrapper.java

--- VirtualAugmentation & Android - Meme generator [SoftCentre, Sweden]/app/src/flavorAtest/flavorGplay/java/net/gsantner/opoc/util/AndroidSupportMeWrapper.java
+++ VirtualAugmentation & Android - Meme generator [SoftCentre, Sweden]/app/src/flavorAtest/flavorGplay/java/net/gsantner/opoc/util/AndroidSupportMeWrapper.java
@@ -6,7 +6,7 @@
  *   License of this file: Apache 2.0 (Commercial upon request)  
  *     https://www.apache.org/licenses/LICENSE-2.0
  *     https://github.com/gsantner/opoc/#licensing
- *
+ * 
 #########################################################*/
 package net.gsantner.opoc.util;
 
